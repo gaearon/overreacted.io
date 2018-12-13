56c44bafdb7b65315f49f654892f885478e35da8
gc <30398469+gach@users.noreply.github.com>
2018-12-13 15:01:56 +1100

    add localStorage helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,4 +1,25 @@
 export function formatReadingTime(minutes) {
-  let cups = Math.round(minutes / 5);
-  return `${new Array(cups || 1).fill('☕️').join('')} ${minutes} min read`;
+  let cups = Math.round(minutes / 5)
+  return `${new Array(cups || 1).fill('☕️').join('')} ${minutes} min read`
+}
+
+export const loadState = key => {
+  try {
+    const serializedState = localStorage.getItem(key)
+    if (serializedState === null) {
+      return undefined
+    }
+    return JSON.parse(serializedState)
+  } catch (err) {
+    return undefined
+  }
+}
+
+export const saveState = (key, state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem(key, serializedState)
+  } catch (err) {
+    return undefined
+  }
 }
