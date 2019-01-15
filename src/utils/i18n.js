0cc9c2d325c6b27c0fe2ba6a0f3aeaeeaf261a32
Leonardo Vizzotto <leonardovizzotto14@gmail.com>
2019-01-15 14:22:23 -0200

    Brazilian portuguese translation of 'Optimized for Change' (#79)
    * Brazilian portuguese translation of 'Optimized for Change'
    
    * Update optimized-for-change.pt-BR.md
    
    * Update optimized-for-change.md
    
    * Update i18n.js
    
    * Update and rename optimized-for-change.pt-BR.md to optimized-for-change.pt-br.md
    
    * Update optimized-for-change.md

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -8,12 +8,15 @@ import { defaultLangKey } from '../../languages'
 // https://www.w3.org/International/articles/language-tags/
 // https://www.iana.org/assignments/language-subtag-registry/language-subtag-registry
 // https://discuss.httparchive.org/t/what-are-the-invalid-uses-of-the-lang-attribute/1022
+
+// Please keep keys lowercase!
 export const codeToLanguage = (code) => ({
   en: 'English',
   ru: 'Russian',
   tr: 'Turkish',
   es: 'Spanish',
   ko: 'Korean',
+  'pt-br': 'Portuguese (Brazil)',
   'zh-hant': 'Chinese (Traditional)',
 }[code])
 
