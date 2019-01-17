f8422e1b58965f480ea4309843f70e9af66c22b3
Maga Alkhasov <maga@alkhasov.com>
2019-01-17 11:20:52 +0400

    added local fonts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,2 @@
-import './src/utils/theme.css';
+import './src/utils/theme.css'
+import './src/utils/fonts.css'
--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,9 +1,5 @@
 import React from 'react'
 
-// Import typefaces
-import 'typeface-montserrat'
-import 'typeface-merriweather'
-
 import profilePic from './profile-pic.jpg'
 import { rhythm } from '../utils/typography'
 
