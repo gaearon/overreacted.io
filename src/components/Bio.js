75f4b505b273e62910fe484fe3ba8075a6dc6d0c
Dan Abramov <dan.abramov@gmail.com>
2019-01-20 01:52:19 +0000

    use jpg profile pic

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -4,7 +4,7 @@ import React from 'react';
 import 'typeface-montserrat';
 import 'typeface-merriweather';
 
-import profilePic from '../assets/profile-pic.png';
+import profilePic from '../assets/profile-pic.jpg';
 import { rhythm } from '../utils/typography';
 
 class Bio extends React.Component {
