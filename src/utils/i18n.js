60cbde5876779a74111dc82dc75cf64ff48f53eb
Rostyslav Rozhko <rostyslav.rozhko@gmail.com>
2019-01-24 21:40:29 +0200

    uk-optimized-for-changes

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -29,6 +29,7 @@ export const codeToLanguage = code =>
     th: 'ไทย',
     my: 'မြန်မာဘာသာ',
     sk: 'Slovenčina',
+    uk: 'Українська',
   }[code].replace(/ /g, ' ' /* nbsp */));
 
 export const loadFontsForCode = code => {
