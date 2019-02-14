a55e3c0593939f201556ef0d3d7020add9010936
Jordan Overbye <jordanoverbye@gmail.com>
2019-02-14 17:07:40 +1000

    Convert to functions after merging and useLayoutEffect

--- src/templates/blog-index.js
+++ src/templates/blog-index.js
@@ -10,72 +10,70 @@ import { formatPostDate, formatReadingTime } from '../utils/helpers';
 import { rhythm } from '../utils/typography';
 import Panel from '../components/Panel';
 
-class BlogIndexTemplate extends React.Component {
-  render() {
-    const siteTitle = get(this, 'props.data.site.siteMetadata.title');
-    const langKey = this.props.pageContext.langKey;
+function BlogIndexTemplate(props) {
+  const siteTitle = get(props, 'data.site.siteMetadata.title');
+  const langKey = props.pageContext.langKey;
 
-    const posts = get(this, 'props.data.allMarkdownRemark.edges').filter(
-      ({ node }) => node.fields.langKey === langKey
-    );
+  const posts = get(props, 'data.allMarkdownRemark.edges').filter(
+    ({ node }) => node.fields.langKey === langKey
+  );
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO />
-        <aside>
-          <Bio />
-        </aside>
-        <main>
-          {langKey !== 'en' && langKey !== 'ru' && (
-            <Panel>
-              These articles have been{' '}
-              <a
-                href="https://github.com/gaearon/overreacted.io#contributing-translations"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                translated by the community
-              </a>
-              .
-            </Panel>
-          )}
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO />
+      <aside>
+        <Bio />
+      </aside>
+      <main>
+        {langKey !== 'en' && langKey !== 'ru' && (
+          <Panel>
+            These articles have been{' '}
+            <a
+              href="https://github.com/gaearon/overreacted.io#contributing-translations"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              translated by the community
+            </a>
+            .
+          </Panel>
+        )}
 
-          {posts.map(({ node }) => {
-            const title = get(node, 'frontmatter.title') || node.fields.slug;
-            return (
-              <article key={node.fields.slug}>
-                <header>
-                  <h3
-                    style={{
-                      fontFamily: 'Montserrat, sans-serif',
-                      fontSize: rhythm(1),
-                      marginBottom: rhythm(1 / 4),
-                    }}
+        {posts.map(({ node }) => {
+          const title = get(node, 'frontmatter.title') || node.fields.slug;
+          return (
+            <article key={node.fields.slug}>
+              <header>
+                <h3
+                  style={{
+                    fontFamily: 'Montserrat, sans-serif',
+                    fontSize: rhythm(1),
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link
+                    style={{ boxShadow: 'none' }}
+                    to={node.fields.slug}
+                    rel="bookmark"
                   >
-                    <Link
-                      style={{ boxShadow: 'none' }}
-                      to={node.fields.slug}
-                      rel="bookmark"
-                    >
-                      {title}
-                    </Link>
-                  </h3>
-                  <small>
-                    {formatPostDate(node.frontmatter.date, 'en')}
-                    {` • ${formatReadingTime(node.timeToRead)}`}
-                  </small>
-                </header>
-                <p
-                  dangerouslySetInnerHTML={{ __html: node.frontmatter.spoiler }}
-                />
-              </article>
-            );
-          })}
-        </main>
-        <Footer />
-      </Layout>
-    );
-  }
+                    {title}
+                  </Link>
+                </h3>
+                <small>
+                  {formatPostDate(node.frontmatter.date, 'en')}
+                  {` • ${formatReadingTime(node.timeToRead)}`}
+                </small>
+              </header>
+              <p
+                dangerouslySetInnerHTML={{ __html: node.frontmatter.spoiler }}
+              />
+            </article>
+          );
+        })}
+      </main>
+      <Footer />
+    </Layout>
+  );
 }
 
 export default BlogIndexTemplate;
--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -22,219 +22,213 @@ const systemFont = `system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
     "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans",
     "Droid Sans", "Helvetica Neue", sans-serif`;
 
-class Translations extends React.Component {
-  render() {
-    let { translations, lang, languageLink, editUrl } = this.props;
+function Translations({ translations, lang, languageLink, editUrl }) {
+  let readerTranslations = translations.filter(lang => lang !== 'ru');
+  let hasRussianTranslation = translations.indexOf('ru') !== -1;
 
-    let readerTranslations = translations.filter(lang => lang !== 'ru');
-    let hasRussianTranslation = translations.indexOf('ru') !== -1;
-
-    return (
-      <div className="translations">
-        <Panel style={{ fontFamily: systemFont }}>
-          {translations.length > 0 && (
-            <span>
-              {hasRussianTranslation && (
-                <span>
-                  Originally written in:{' '}
-                  {'en' === lang ? (
-                    <b>{codeToLanguage('en')}</b>
-                  ) : (
-                    <Link to={languageLink('en')}>English</Link>
-                  )}
-                  {' • '}
-                  {'ru' === lang ? (
-                    <b>Русский (авторский перевод)</b>
-                  ) : (
-                    <Link to={languageLink('ru')}>
-                      Русский (авторский перевод)
-                    </Link>
-                  )}
-                  <br />
-                  <br />
-                </span>
-              )}
-              <span>Translated by readers into: </span>
-              {readerTranslations.map((l, i) => (
-                <React.Fragment key={l}>
-                  {l === lang ? (
-                    <b>{codeToLanguage(l)}</b>
-                  ) : (
-                    <Link to={languageLink(l)}>{codeToLanguage(l)}</Link>
-                  )}
-                  {i === readerTranslations.length - 1 ? '' : ' • '}
-                </React.Fragment>
-              ))}
-            </span>
-          )}
-          {lang !== 'en' && lang !== 'ru' && (
-            <>
-              <br />
-              <br />
-              <Link to={languageLink('en')}>Read the original</Link>
-              {' • '}
-              <a href={editUrl} target="_blank" rel="noopener noreferrer">
-                Improve this translation
-              </a>
-              {' • '}
-              <Link to={`/${lang}`}>View all translated posts</Link>{' '}
-            </>
-          )}
-        </Panel>
-      </div>
-    );
-  }
+  return (
+    <div className="translations">
+      <Panel style={{ fontFamily: systemFont }}>
+        {translations.length > 0 && (
+          <span>
+            {hasRussianTranslation && (
+              <span>
+                Originally written in:{' '}
+                {'en' === lang ? (
+                  <b>{codeToLanguage('en')}</b>
+                ) : (
+                  <Link to={languageLink('en')}>English</Link>
+                )}
+                {' • '}
+                {'ru' === lang ? (
+                  <b>Русский (авторский перевод)</b>
+                ) : (
+                  <Link to={languageLink('ru')}>
+                    Русский (авторский перевод)
+                  </Link>
+                )}
+                <br />
+                <br />
+              </span>
+            )}
+            <span>Translated by readers into: </span>
+            {readerTranslations.map((l, i) => (
+              <React.Fragment key={l}>
+                {l === lang ? (
+                  <b>{codeToLanguage(l)}</b>
+                ) : (
+                  <Link to={languageLink(l)}>{codeToLanguage(l)}</Link>
+                )}
+                {i === readerTranslations.length - 1 ? '' : ' • '}
+              </React.Fragment>
+            ))}
+          </span>
+        )}
+        {lang !== 'en' && lang !== 'ru' && (
+          <>
+            <br />
+            <br />
+            <Link to={languageLink('en')}>Read the original</Link>
+            {' • '}
+            <a href={editUrl} target="_blank" rel="noopener noreferrer">
+              Improve this translation
+            </a>
+            {' • '}
+            <Link to={`/${lang}`}>View all translated posts</Link>{' '}
+          </>
+        )}
+      </Panel>
+    </div>
+  );
 }
 
-class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark;
-    const siteTitle = get(this.props, 'data.site.siteMetadata.title');
-    let {
-      previous,
-      next,
-      slug,
-      translations,
-      translatedLinks,
-    } = this.props.pageContext;
-    const lang = post.fields.langKey;
+function BlogPostTemplate(props) {
+  const post = props.data.markdownRemark;
+  const siteTitle = get(props, 'data.site.siteMetadata.title');
+  let {
+    previous,
+    next,
+    slug,
+    translations,
+    translatedLinks,
+  } = props.pageContext;
+  const lang = post.fields.langKey;
 
-    // Replace original links with translated when available.
-    let html = post.html;
-    translatedLinks.forEach(link => {
-      // jeez
-      function escapeRegExp(str) {
-        return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
-      }
-      let translatedLink = '/' + lang + link;
-      html = html.replace(
-        new RegExp('"' + escapeRegExp(link) + '"', 'g'),
-        '"' + translatedLink + '"'
-      );
-    });
-
-    translations = translations.slice();
-    translations.sort((a, b) => {
-      return codeToLanguage(a) < codeToLanguage(b) ? -1 : 1;
-    });
-
-    loadFontsForCode(lang);
-    // TODO: this curried function is annoying
-    const languageLink = createLanguageLink(slug, lang);
-    const enSlug = languageLink('en');
-    const editUrl = `https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO_NAME}/edit/master/src/pages/${enSlug.slice(
-      1,
-      enSlug.length - 1
-    )}/index${lang === 'en' ? '' : '.' + lang}.md`;
-    const discussUrl = `https://mobile.twitter.com/search?q=${encodeURIComponent(
-      `https://overreacted.io${enSlug}`
-    )}`;
-
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO
-          lang={lang}
-          title={post.frontmatter.title}
-          description={post.frontmatter.spoiler}
-          slug={post.fields.slug}
-        />
-        <main>
-          <article>
-            <header>
-              <h1 style={{ color: 'var(--textTitle)' }}>
-                {post.frontmatter.title}
-              </h1>
-              <p
-                style={{
-                  ...scale(-1 / 5),
-                  display: 'block',
-                  marginBottom: rhythm(1),
-                  marginTop: rhythm(-4 / 5),
-                }}
-              >
-                {formatPostDate(post.frontmatter.date, lang)}
-                {` • ${formatReadingTime(post.timeToRead)}`}
-              </p>
-              {translations.length > 0 && (
-                <Translations
-                  translations={translations}
-                  editUrl={editUrl}
-                  languageLink={languageLink}
-                  lang={lang}
-                />
-              )}
-            </header>
-            <div dangerouslySetInnerHTML={{ __html: html }} />
-            <footer>
-              <p>
-                <a href={discussUrl} target="_blank" rel="noopener noreferrer">
-                  Discuss on Twitter
-                </a>
-                {` • `}
-                <a href={editUrl} target="_blank" rel="noopener noreferrer">
-                  Edit on GitHub
-                </a>
-              </p>
-            </footer>
-          </article>
-        </main>
-        <aside>
-          <div
-            style={{
-              margin: '90px 0 40px 0',
-              fontFamily: systemFont,
-            }}
-          >
-            <Signup />
-          </div>
-          <h3
-            style={{
-              fontFamily: 'Montserrat, sans-serif',
-              marginTop: rhythm(0.25),
-            }}
-          >
-            <Link
-              style={{
-                boxShadow: 'none',
-                textDecoration: 'none',
-                color: 'var(--pink)',
-              }}
-              to={'/'}
-            >
-              Overreacted
-            </Link>
-          </h3>
-          <Bio />
-          <nav>
-            <ul
-              style={{
-                display: 'flex',
-                flexWrap: 'wrap',
-                justifyContent: 'space-between',
-                listStyle: 'none',
-                padding: 0,
-              }}
-            >
-              <li>
-                {previous && (
-                  <Link to={previous.fields.slug} rel="prev">
-                    ← {previous.frontmatter.title}
-                  </Link>
-                )}
-              </li>
-              <li>
-                {next && (
-                  <Link to={next.fields.slug} rel="next">
-                    {next.frontmatter.title} →
-                  </Link>
-                )}
-              </li>
-            </ul>
-          </nav>
-        </aside>
-      </Layout>
+  // Replace original links with translated when available.
+  let html = post.html;
+  translatedLinks.forEach(link => {
+    // jeez
+    function escapeRegExp(str) {
+      return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    }
+    let translatedLink = '/' + lang + link;
+    html = html.replace(
+      new RegExp('"' + escapeRegExp(link) + '"', 'g'),
+      '"' + translatedLink + '"'
     );
-  }
+  });
+
+  translations = translations.slice();
+  translations.sort((a, b) => {
+    return codeToLanguage(a) < codeToLanguage(b) ? -1 : 1;
+  });
+
+  loadFontsForCode(lang);
+  // TODO: this curried function is annoying
+  const languageLink = createLanguageLink(slug, lang);
+  const enSlug = languageLink('en');
+  const editUrl = `https://github.com/${GITHUB_USERNAME}/${GITHUB_REPO_NAME}/edit/master/src/pages/${enSlug.slice(
+    1,
+    enSlug.length - 1
+  )}/index${lang === 'en' ? '' : '.' + lang}.md`;
+  const discussUrl = `https://mobile.twitter.com/search?q=${encodeURIComponent(
+    `https://overreacted.io${enSlug}`
+  )}`;
+
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO
+        lang={lang}
+        title={post.frontmatter.title}
+        description={post.frontmatter.spoiler}
+        slug={post.fields.slug}
+      />
+      <main>
+        <article>
+          <header>
+            <h1 style={{ color: 'var(--textTitle)' }}>
+              {post.frontmatter.title}
+            </h1>
+            <p
+              style={{
+                ...scale(-1 / 5),
+                display: 'block',
+                marginBottom: rhythm(1),
+                marginTop: rhythm(-4 / 5),
+              }}
+            >
+              {formatPostDate(post.frontmatter.date, lang)}
+              {` • ${formatReadingTime(post.timeToRead)}`}
+            </p>
+            {translations.length > 0 && (
+              <Translations
+                translations={translations}
+                editUrl={editUrl}
+                languageLink={languageLink}
+                lang={lang}
+              />
+            )}
+          </header>
+          <div dangerouslySetInnerHTML={{ __html: html }} />
+          <footer>
+            <p>
+              <a href={discussUrl} target="_blank" rel="noopener noreferrer">
+                Discuss on Twitter
+              </a>
+              {` • `}
+              <a href={editUrl} target="_blank" rel="noopener noreferrer">
+                Edit on GitHub
+              </a>
+            </p>
+          </footer>
+        </article>
+      </main>
+      <aside>
+        <div
+          style={{
+            margin: '90px 0 40px 0',
+            fontFamily: systemFont,
+          }}
+        >
+          <Signup />
+        </div>
+        <h3
+          style={{
+            fontFamily: 'Montserrat, sans-serif',
+            marginTop: rhythm(0.25),
+          }}
+        >
+          <Link
+            style={{
+              boxShadow: 'none',
+              textDecoration: 'none',
+              color: 'var(--pink)',
+            }}
+            to={'/'}
+          >
+            Overreacted
+          </Link>
+        </h3>
+        <Bio />
+        <nav>
+          <ul
+            style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'space-between',
+              listStyle: 'none',
+              padding: 0,
+            }}
+          >
+            <li>
+              {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                  ← {previous.frontmatter.title}
+                </Link>
+              )}
+            </li>
+            <li>
+              {next && (
+                <Link to={next.fields.slug} rel="next">
+                  {next.frontmatter.title} →
+                </Link>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </aside>
+    </Layout>
+  );
 }
 
 export default BlogPostTemplate;
