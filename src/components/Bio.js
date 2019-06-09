78b5fee3196974667d16a6f24e970e4de102c6d6
Benjamin Cooper <bencooper222@gmail.com>
2019-06-09 15:10:45 -0700

    switch profile picture

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import profilePic from '../assets/profile-pic.jpg';
+import profilePic from '../assets/me.png';
 import { rhythm } from '../utils/typography';
 
 class Bio extends React.Component {
@@ -13,7 +13,7 @@ class Bio extends React.Component {
       >
         <img
           src={profilePic}
-          alt={`Dan Abramov`}
+          alt={`Benjamin Cooper`}
           style={{
             marginRight: rhythm(1 / 2),
             marginBottom: 0,
@@ -22,10 +22,9 @@ class Bio extends React.Component {
             borderRadius: '50%',
           }}
         />
-        <p style={{ maxWidth: 310 }}>
-          Personal blog by{' '}
-          <a href="https://mobile.twitter.com/dan_abramov">Dan Abramov</a>.{' '}
-          I&nbsp;explain with words and code.
+        <p style={{ maxWidth: 370 }}>
+          <a href="https://twitter.com/bcoops222">Ben Cooper's</a> blog. <br />{' '}
+          I write mostly about code, policy, and my life.
         </p>
       </div>
     );
