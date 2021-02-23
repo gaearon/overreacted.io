e80a8a14f54959e5593a44fbf8f6c814cfeee5ed
Dan Abramov <dan.abramov@gmail.com>
2021-02-23 18:17:05 +0000

    Maybe fix links (#687)
    * Maybe fix links
    
    * Update i18n.js

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -67,7 +67,7 @@ export const createLanguageLink = (slug, lang) => {
   const rawSlug = slug.replace(`${lang}/`, '');
 
   return targetLang =>
-    targetLang === 'en' ? rawSlug : `${targetLang}${rawSlug}`;
+    targetLang === 'en' ? rawSlug : `/${targetLang}${rawSlug}`;
 };
 
 export const replaceAnchorLinksByLanguage = (html, code) => {
