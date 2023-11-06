84b37a9352dbcd8fa0ea4e9a5eb5350a326affec
Dan Abramov <dan.abramov@gmail.com>
2023-11-06 03:07:19 +0000

    styling for 404

--- app/not-found.js
+++ app/not-found.js
@@ -1,10 +1,34 @@
+import { sans } from "./fonts";
+import Link from "./Link";
+
 export default function NotFound() {
   return (
-    <>
-      <h1>Not found.</h1>
-      <p>
-        If you're looking for translated posts, they're not on the site anymore but <a href="https://github.com/gaearon/overreacted.io/tree/archive/src/pages">you can find an archive of them here.</a>
-      </p>
-    </>
-  )
+    <article className="markdown">
+      <h1
+        className={[
+          sans.className,
+          "text-[40px] font-black leading-[44px] text-[--title]",
+        ].join(" ")}
+      >
+        Not found
+      </h1>
+      <div className="markdown mt-10">
+        <p>This page doesn't exist (yet?)</p>
+        <p>
+          I recently rewrote the site so maybe something broke. Please{" "}
+          <Link href="https://github.com/gaearon/overreacted.io/issues/796">
+            complain here.
+          </Link>
+        </p>
+        <p>
+          If you're looking for translated posts, they're not on the site
+          anymore but you can find{" "}
+          <Link href="https://github.com/gaearon/overreacted.io/tree/archive/src/pages">
+            an archive of them here.
+          </Link>
+        </p>
+        <p>Hope you'll find what you're looking for.</p>
+      </div>
+    </article>
+  );
 }
