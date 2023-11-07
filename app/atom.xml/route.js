1a1e5a82052c722f5c3a1d9aa96fbe795ae82124
Christian Mund <git@mundpropaganda.net>
2023-11-07 10:51:27 +0100

    fix indent

--- app/atom.xml/route.js
+++ app/atom.xml/route.js
@@ -2,7 +2,7 @@ import { getPosts, metadata } from "../page";
 import { generateFeed } from "../feed";
 
 export async function GET() {
-	const posts = await getPosts();
-	const feed = generateFeed(posts, metadata);
-	return new Response(feed.atom1());
+  const posts = await getPosts();
+  const feed = generateFeed(posts, metadata);
+  return new Response(feed.atom1());
 }
--- app/rss.xml/route.js
+++ app/rss.xml/route.js
@@ -2,7 +2,7 @@ import { getPosts, metadata } from "../page";
 import { generateFeed } from "../feed";
 
 export async function GET() {
-	const posts = await getPosts();
-	const feed = generateFeed(posts, metadata);
-	return new Response(feed.rss2());
+  const posts = await getPosts();
+  const feed = generateFeed(posts, metadata);
+  return new Response(feed.rss2());
 }
