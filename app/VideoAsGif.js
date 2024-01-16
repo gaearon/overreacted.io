7cab0131a4324854f52ac8d3329a56c06d75c056
Alex Andru <alex007d@gmail.com>
2024-01-16 23:34:01 +0100

    Add prop destructuring to VideoAsGif

--- app/VideoAsGif.js
+++ app/VideoAsGif.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react";
 
 export function VideoAsGif(props) {
+    const {src, ariaLabel} = props
     const [mounted, setMounted] = useState(false)
     useEffect(()=>{
         setMounted(true)
@@ -10,6 +11,6 @@ export function VideoAsGif(props) {
     if(!mounted){
         return null
     }
-return <video className="mb-5" muted autoPlay loop playsInline aria-label={props.ariaLabel}><source src={props.src} type="video/mp4"/></video>
+return <video className="mb-5" muted autoPlay loop playsInline aria-label={ariaLabel}><source src={src} type="video/mp4"/></video>
 
 }
