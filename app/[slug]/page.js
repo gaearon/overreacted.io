52782a5a5ddbb02714c4425b2d480f353c207dbf
Aaron Young <luozhu2021@gmail.com>
2025-04-01 08:44:29 +0800

    Style Fix

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -25,7 +25,7 @@ export default async function PostPage({ params }) {
   }
   const { content, data } = matter(file);
   const editUrl = `https://github.com/youngjuning/weekly/edit/main/public/${encodeURIComponent(
-    params.slug,
+    slug,
   )}/index.md`;
 
   return (
