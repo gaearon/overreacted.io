c15da8e4b091a30497c2de5600044d38a822aba4
Aaron Young <luozhu2021@gmail.com>
2025-04-01 14:25:57 +0800

    chore: update google ads script in layout.js

--- app/layout.js
+++ app/layout.js
@@ -35,7 +35,11 @@ export default function RootLayout({ children }) {
           gtag('config', 'G-L0YQ2HEGZJ');
         `}
         </Script>
-        <Script strategy="afterInteractive" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></Script>
+        <Script strategy="afterInteractive" src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></Script><Script strategy="afterInteractive">
+        {`
+          (adsbygoogle = window.adsbygoogle || []).push({});
+        `}
+      </Script>
       </head>
       <body className="mx-auto max-w-2xl bg-[--bg] px-5 py-12 text-[--text]">
         <header className="mb-14 flex flex-row place-content-between">
