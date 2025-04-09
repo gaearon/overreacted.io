313c5627fa1147e18bd6d687b85aad954768dc03
Dan Abramov <dan.abramov@gmail.com>
2025-04-10 03:32:00 +0900

    try adding plausilbe?

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import PlausibleProvider from "next-plausible";
 import Link from "./Link";
 import HomeLink from "./HomeLink";
 import { serif } from "./fonts";
@@ -7,20 +8,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={serif.className}>
       <body className="mx-auto max-w-2xl bg-[--bg] px-5 py-12 text-[--text]">
-        <header className="mb-14 flex flex-row place-content-between">
-          <HomeLink />
-          <span className="relative top-[4px] italic">
-            by{" "}
-            <Link href="https://danabra.mov" target="_blank">
-              <img
-                alt="Dan Abramov"
-                src="https://github.com/gaearon.png"
-                className="relative -top-1 mx-1 inline h-8 w-8 rounded-full"
-              />
-            </Link>
-          </span>
-        </header>
-        <main>{children}</main>
+        <PlausibleProvider domain="overreacted.io">
+          <header className="mb-14 flex flex-row place-content-between">
+            <HomeLink />
+            <span className="relative top-[4px] italic">
+              by{" "}
+              <Link href="https://danabra.mov" target="_blank">
+                <img
+                  alt="Dan Abramov"
+                  src="https://github.com/gaearon.png"
+                  className="relative -top-1 mx-1 inline h-8 w-8 rounded-full"
+                />
+              </Link>
+            </span>
+          </header>
+          <main>{children}</main>
+        </PlausibleProvider>
       </body>
     </html>
   );
