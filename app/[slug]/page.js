4310c7e60f0a10147af336012efd87a49237eccd
Aaron Young <luozhu2021@gmail.com>
2025-04-14 09:53:52 +0800

    Update page.js

--- app/[slug]/page.js
+++ app/[slug]/page.js
@@ -47,20 +47,6 @@ export default async function PostPage({ params }) {
           year: "numeric",
         })}
       </p>
-      <ins
-        className="adsbygoogle"
-        style={{display: "block"}}
-        data-ad-client="ca-pub-5641491107630454"
-        data-ad-slot="1206633556"
-        data-page-url="https://www.nablepart.com"
-        data-override-format="true"
-        data-ad-format="auto"
-        data-full-width-responsive="true"
-      /><Script>
-        {`
-          (adsbygoogle = window.adsbygoogle || []).push({});
-        `}
-      </Script>
       <div className="markdown mt-10">
         <MDXRemote
           source={content}
