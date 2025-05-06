db082c20b3407b4f051f1463138b1413712e80d0
Dan Abramov <dan.abramov@gmail.com>
2025-05-06 14:40:24 +0100

    trim unused

--- app/fonts.js
+++ app/fonts.js
@@ -1,15 +1,15 @@
-import { Montserrat, Merriweather } from 'next/font/google'
+import { Montserrat, Merriweather } from "next/font/google";
 
 export const sans = Montserrat({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['400', '700', '900'],
-  style: ['normal'],
-})
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["900"],
+  style: ["normal"],
+});
 
 export const serif = Merriweather({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['400', '700'],
-  style: ['normal', 'italic'],
-})
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+});
